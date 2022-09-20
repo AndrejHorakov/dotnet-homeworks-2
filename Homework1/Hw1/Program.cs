d72ae263112f0a1f0351308379dd77f1776f284f
AndrewKhoryakov <andrejhorakov47232@gmail.com>
2022-09-20 22:16:22 +0300

    fix problems

--- Homework1/Hw1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Hw1;
-
-double arg1;
-CalculatorOperation operation;
-double arg2;
-
-Parser.ParseCalcArguments(args, out arg1, out operation, out arg2);
-Console.WriteLine(Calculator.Calculate(arg1, operation, arg2));
