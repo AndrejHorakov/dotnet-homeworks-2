4e96d121b8624a4ee47dcf609027bb775bee9093
AndrewKhoryakov <andrejhorakov47232@gmail.com>
2022-10-08 19:27:20 +0300

    HW4

--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,8 @@
-﻿raise (System.NotImplementedException())
+﻿open System
+open Hw4
+
+let main (args : string[]) =
+    let parsedArgs = Parser.parseCalcArguments args
+    let ans = Calculator.calculate parsedArgs.arg1 parsedArgs.operation parsedArgs.arg2
+    printf $"{ans}"
+    
