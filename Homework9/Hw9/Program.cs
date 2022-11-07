37c7fa1ee58b98b33992b33aff56bfe242d45539
AndrewKhoryakov <andrejhorakov47232@gmail.com>
2022-11-07 15:40:58 +0300

    [fix]

--- Homework9/Hw9/Program.cs
+++ Homework9/Hw9/Program.cs
@@ -1,45 +1,40 @@
 using System.Diagnostics.CodeAnalysis;
 using Hw9.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Hw9;
 
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddMathCalculator();
-
-var app = builder.Build();
-
-namespace Hw9
+[ExcludeFromCodeCoverage]
+public class Program
 {
-    public partial class Program
+    public static void Main(string[] args)
     {
-        [ExcludeFromCodeCoverage]
-        static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddMathCalculator();
+        
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddControllersWithViews();
-
-            builder.Services.AddMathCalculator();
-
-            var app = builder.Build();
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Calculator}/{action=Calculator}/{id?}");
-            app.Run();
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Calculator}/{action=Calculator}/{id?}");
+
+        app.Run();
     }
 }
