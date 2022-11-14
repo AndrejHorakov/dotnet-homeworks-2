0c29814681689af7894259fcd2390678c0f77ca4
AndrejHorakov <112895948+AndrejHorakov@users.noreply.github.com>
2022-11-14 05:43:09 +0300

    Update Program.cs

--- Homework9/Hw9/Program.cs
+++ Homework9/Hw9/Program.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using Hw9.Configuration;
-[assembly: ExcludeFromCodeCoverage];
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
